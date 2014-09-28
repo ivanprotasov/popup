e8ee9601e3792c8f50d4ba41dbe2f02bb0c9c496
ivanprotasov <ivanprotasov@narod.ru>
2014-09-28 13:55:48 +0300

    JS commit

--- plugin/popup.jquery.js
+++ plugin/popup.jquery.js
@@ -2,24 +2,56 @@
 	$.fn.setPopup = function (content) {
 		return this.each(function () {
 			$(this).on('click', function(){
-				var $elem = $('<div class="ui-popup-hider"><div class="ui-table"><div class="ui-inner-wrapper">');
-				var $innerElem = $('<div class="ui-popup-content">');
-				$innerElem.append(content);
-				$elem.find('.ui-inner-wrapper').append($innerElem);
+				$window = $(window);
+				$body = $('body');
 
-				$('body').addClass('ui-popup-open')
-						.append($elem)
-						.on('click', '.ui-popup-hider', onHiderClick);
+				var $hider = $('<div class="ui-popup-hider">');
+				var $content = $('<div class="ui-popup-content">');
+				$content.append(content);
+				$hider.append($content);
+
+				$body.addClass('ui-popup-open')
+						.append($hider)
+						.on('click', '.ui-popup-hider', onHiderClick)
+						.on('click', '.ui-popup-content', onInnerElemClick);
+
+				var innerElemWidth = $content.outerWidth();
+				var innerElemHeight = $content.outerHeight();
+
+				setPosition();
+
+				$window.resize(setPosition);
+
+				function setPosition(){
+					var windowHeight = $window.height();
+					var windowWidth = $window.width();
+
+
+					var marginTopPosition=(innerElemHeight > windowHeight)?30:(windowHeight-innerElemHeight)/2;
+					var marginLeftPosition=(innerElemWidth > windowWidth)?30:(windowWidth-innerElemWidth)/2;
+
+					$content.css({
+						marginTop: marginTopPosition,
+						marginLeft: marginLeftPosition
+					})
+				}
+
+				function onHiderClick(){
+					$hider.remove();
+					$body.removeClass('ui-popup-open')
+						.off('click', '.ui-popup-hider',onHiderClick)
+						.off('click', '.ui-popup-hider',onInnerElemClick);
+					$window.off("resize", setPosition);
+				}
+
+				function onInnerElemClick(e){
+					e.stopPropagation();
+				}
 
 			});
 
 		});
 
-		function onHiderClick(){
-			$('.ui-popup-hider').remove();
-			$('body').removeClass('ui-popup-open')
-					.off('click', '.ui-popup-hider',onHiderClick);
-		}
 	};
 
 })(jQuery);
