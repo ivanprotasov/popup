255a95e567ee46f36de19c49e2907864e311d31c
ivanprotasov <ivanprotasov@narod.ru>
2014-09-28 12:33:50 +0300

    Table commit

--- plugin/popup.jquery.js
+++ plugin/popup.jquery.js
@@ -2,12 +2,15 @@
 	$.fn.setPopup = function (content) {
 		return this.each(function () {
 			$(this).on('click', function(){
+				var $elem = $('<div class="ui-popup-hider"><div class="ui-table"><div class="ui-inner-wrapper">');
+				var $innerElem = $('<div class="ui-popup-content">');
+				$innerElem.append(content);
+				$elem.find('.ui-inner-wrapper').append($innerElem);
+
 				$('body').addClass('ui-popup-open')
-						.append('<div class="ui-popup-hider">')
+						.append($elem)
 						.on('click', '.ui-popup-hider', onHiderClick);
 
-				$('.ui-popup-hider').append('<div class="ui-popup-content">');
-				$('.ui-popup-content').html(content)
 			});
 
 		});
